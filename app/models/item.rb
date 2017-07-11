bd28629ccc6682b6bf4200362f0b33e1ea7f78ed
elkhann <emilkhann@gmail.com>
2017-07-11 14:03:36 +0300

    End edit & update

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,13 +3,12 @@ class Item < ApplicationRecord
               numericality: { greater_than: 0, allow_nil: true} #Цена не может быть 0, монж создать пустую
   validates :name, :description, presence: true # Обязательно должно быть задано
 
-  belongs_to :category
 
   after_initialize { } # Item.new; Item.first
   after_save       { } # Item.save; Item.create; item.update_attributes ()
-  after_create     { category.inc(:items_count,  1) }
+  after_create     { }
   after_update     { }
-  after_destroy    { category.inc(:items_count, -1) } # item.destroy
+  after_destroy    { } # item.destroy
 
 
 end
